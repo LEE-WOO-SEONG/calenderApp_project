52156fe59d1c736a6d8548a8f28e53dc35b22d15
soda-cloud <linn45577@gmail.com>
2020-06-24 21:17:27 +0900

    feat: add dep function

--- public/src/js/function.js
+++ public/src/js/function.js
@@ -6,13 +6,18 @@ let schedules = [];
 
 // scheduleRender
 function schedulesRender() {
-  // 4. length값으로 sort
+  // 토요일이 있는지 확인
+  // function checksaturday() {
+  //   const saturday = [];
+  // }
+
+  // length값으로 sort
   function compare(length) {
     return (a, b) => (a[length] < b[length] ? 1 : a[length] > b[length] ? -1 : 0);
   }
   schedules.sort(compare('length'));
   console.log(schedules);
-  // 5. dep 조정
+  // dep 조정
   function schedulesOrder() {
     let dep = 0;
     for (let i = 0; i < schedules.length; i++) {
@@ -21,10 +26,10 @@ function schedulesRender() {
       } else {
         for (let k = 1; k < i + 1; k++) {
           if (schedules[i - k].from <= schedules[i].from && schedules[i].from <= schedules[i - k].to) {
-            ++dep; console.log(schedules[i]);
+            ++dep;
           }
         }
-        console.log(dep);
+        console.log('[dep조정]', dep, schedules[i]);
         render(schedules[i], dep);
         dep = 0;
       }
@@ -33,12 +38,13 @@ function schedulesRender() {
 
   // 6. render
   function render(schedule, dep) {
+    // console.log(schedule, dep);
     if (!document.getElementById(`${schedule.from}`)) return;
     const $inner = document.getElementById(`${schedule.from}`);
-    $inner.querySelector('.schedule-inner-container').innerHTML += `<div class="schedule-list" role="button">${
-      schedule.title}</div>`;
-    $inner.querySelector('.schedule-list').style.width = `${95 * (schedule.length + 1)}%`;
-    $inner.querySelector('.schedule-list').style.transform = `translateY(${dep * 110}%)`;
+    $inner.querySelector('.schedule-inner-container').innerHTML += `<div id="${schedule.id}" class="schedule-list" role="button">${
+      schedule.title}<span class="schedule-id>${schedule.id}</span></div>`;
+    document.getElementById(`${schedule.id}`).style.width = `${95 * (schedule.length + 1)}%`;
+    document.getElementById(`${schedule.id}`).style.transform = `translateY(${dep * 110}%)`;
   }
   schedulesOrder();
 }
